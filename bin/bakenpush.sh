dfdf0f7f2ef0761d23366cbd5f4b3053b6fb6413
Atamert Ölçgen <muhuk@muhuk.com>
2018-02-07 13:51:44 +0800

    Check Internet connection before trying to sync

--- bin/bakenpush.sh
+++ bin/bakenpush.sh
@@ -17,16 +17,34 @@ WORK_DIR=$1
 CURR_DIR=$(pwd)
 QUIET_PERIOD=$2
 
+# Bastardized from https://stackoverflow.com/q/929368/42188
+function am_i_online() {
+    rc=0
+    for interface in $(ls /sys/class/net/ | grep -v lo);
+    do
+    	if [[ $(cat /sys/class/net/$interface/carrier 2> /dev/null) = 1 ]]; then
+    	    rc=1;
+    	fi
+    done
+    return $rc;
+}
+
+
 cd $WORK_DIR
 
 flashbake -q . $QUIET_PERIOD
 
-git fetch -q
-if [[ $(git log --oneline HEAD..FETCH_HEAD) ]]; then
-    git rebase -q
+am_i_online
+if [ "$?" -eq 1 ]; then
+    git fetch -q
+    if [[ $(git log --oneline HEAD..FETCH_HEAD) ]]; then
+	git rebase -q
+    fi
+    if [[ $(git log --oneline FETCH_HEAD..HEAD) ]]; then
+	git push -q
+    fi
+else
+    echo "no Internet connection!"
 fi
-if [[ $(git log --oneline FETCH_HEAD..HEAD) ]]; then
-    git push -q
-fi
-
+    
 cd $CURR_DIR
