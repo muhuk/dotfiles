52ebae4afc6c269fa7118ba91f92bc653e32c68e
Atamert Ölçgen <muhuk@muhuk.com>
2022-08-24 13:26:59 +0300

    Add key config for vertical mouse.

--- bin/remap-middle-mouse-button.sh
+++ bin/remap-middle-mouse-button.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 
+# Razer Setup
+
 DEVICE_NAME="Razer Razer Mamba Tournament Edition"
 
 for id in $(xinput --list | \
@@ -8,3 +10,13 @@ do
     xinput set-button-map $id 1 2 3 4 5 6 7 8 2
 done
 
+
+# Delux Setup
+
+DEVICE_NAME="E-Signal USB Gaming Mouse"
+
+for id in $(xinput --list | \
+            sed -n "/$DEVICE_NAME.*pointer/s/.*=\([0-9]\+\).*/\1/p")
+do
+    xinput set-button-map $id 1 2 3 4 5 6 7 2 9
+done
